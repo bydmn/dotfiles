039cce4cf6334fc93811d403324ad9532605d7f7
yoshino-birdman <yoshino.birdman@gmail.com>
2019-03-23 14:45:18 +0900

    homebrewの初期化処理を修正

--- etc/init/init-brew.sh
+++ etc/init/init-brew.sh
@@ -3,7 +3,8 @@ if [ "$(uname)" == 'Darwin' ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew doctor
   brew update
-  brew install caskroom/cask/brew-cask
+  brew tap caskroom/cask
+  brew install rcmdnk/file/brew-file
   . $HOME/.bashrc
   brew file install
 fi
